2f5d5738d6352a9dae2035239309572ddb9401c4
oqgraphic <hello@oqgraphic.com>
2019-12-24 20:01:07 +0100

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <div>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi motherfuckers!</h1>
+    <p>Welcome to my nasty place</p>
     <p>Now go build something great.</p>
- 
+
     <Link to="/blog/">Go to my blog</Link> <br />
-    <Link to="/about/">Go to my about page</Link> <br />
-    <Link to="/contact/">Go to my contact page</Link> <br />
+    <Link to="/about/">About</Link> <br />
+    <Link to="/contact/">Contact</Link> <br />
     <Link to="/404/">404</Link> <br/>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Page two</Link>
   </div>
 )
 
